66eb46946e104241a92d5eeca3dfb760014f9791
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-03-19 09:50:22 -0600

    middleware added

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,5 @@
-/* import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-	function middleware(req) {
-		if (!req.nextauth.token) {
-			const url = req.nextUrl.clone();
-			url.pathname = "/login";
-			url.searchParams.set("callbackUrl", req.nextUrl.pathname);
-			return NextResponse.redirect(url);
-		}
-	},
-	{
-		callbacks: {
-			authorized: ({ token }) => !!token,
-		},
-	}
-);
+export { default } from "next-auth/middleware";
 
 export const config = {
-	matcher: ["/fnfg", "/today", "/profile"],
+	matcher: ["/profile", "/today"],
 };
- */
